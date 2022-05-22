4e2d00cf25a3aa6e8b16790c5cf4e6f78ea19b4d
Jailson Paiva <j.psherl1009@gmail.com>
2022-05-22 11:52:49 -0400

    pagina de acesso negado adicionada

--- server.js
+++ server.js
@@ -36,21 +36,19 @@ const port = process.env.PORT || 3000;
 
     app.post('/ler', (req, res) => {
         const ra = req.body.ra
-        console.log(ra)
+        // console.log(ra)
 
         if(!ra) {
             res.render('Preencha os campos')
         } else {
             Aluno.findOne({ra: ra}).then((aluno) => {
                 console.log(ra)
-                const alunos = aluno
+                // const alunos = aluno
                //  VERIFICAÇÃO SE O ALUNO É INSCRITO NO CONGRESSO
                 if(aluno.inscrito === 'T') {
                     const NomeAluno = alunos.nome
                     const RaAluno = alunos.ra
                     const CursoAluno = alunos.curso
-
-                    console.log(aluno)
 
                     const NewQrcode = {
                         code: (`${NomeAluno} ${RaAluno} ${CursoAluno}`)
@@ -62,7 +60,9 @@ const port = process.env.PORT || 3000;
                         res.render('verificado', {code: DataCode, aluno: alunos})
                         
                     })
-             }
+                } else {
+                    res.render('negado')
+                }
             }).catch()
         } 
     })
