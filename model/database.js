4d4f06dc083993b033d70593cf8c390a8f0d1c59
Jailson Dos Santos Paiva <jailsonpaiva@desenvolvemt.local>
2022-05-31 12:50:54 -0400

    jsjsdads

--- model/database.js
+++ model/database.js
@@ -1,51 +1,51 @@
-const database = {
+// const database = {
+//     host: "fasipecpa.com.br",
+//     user: "fasipecp_ads",
+//     password: "Ry[gopYTw?Ca@*Hwvr",
+//     database: "fasipecp_ads",
+// };
+
+
+// // DATABASE MYSQL
+// const { Sequelize, DataTypes } = require('sequelize')
+
+// const sequelize = new Sequelize(database.database, database.user, database.password, { 
+//     host: database.host,
+//     dialect: 'mysql'
+// })
+
+// sequelize.authenticate().then(() => {
+//     console.log("Database is initialized")
+// }).catch(err => {
+//     console.log("Falha ao conectar" + err)
+// })
+
+// module.exports = {
+//     Sequelize: Sequelize,
+//     sequelize: sequelize,
+//     DataTypes: DataTypes
+// } 
+
+
+
+const mysql = require("mysql");
+
+const database = mysql.createConnection({
     host: "fasipecpa.com.br",
     user: "fasipecp_ads",
     password: "Ry[gopYTw?Ca@*Hwvr",
+    port: 3306,
     database: "fasipecp_ads",
-};
+    multipleStatements: true
+});
 
-
-// DATABASE MYSQL
-const { Sequelize, DataTypes } = require('sequelize')
-
-const sequelize = new Sequelize(database.database, database.user, database.password, { 
-    host: database.host,
-    dialect: 'mysql'
-})
-
-sequelize.authenticate().then(() => {
-    console.log("Database is initialized")
-}).catch(err => {
-    console.log("Falha ao conectar" + err)
-})
-
-module.exports = {
-    Sequelize: Sequelize,
-    sequelize: sequelize,
-    DataTypes: DataTypes
-} 
-
-
-
-
-// const mysql = require("mysql");
-
-// database.connect((erro) => {
-//     if (erro) {
-//         throw erro;
-//     } else {
-//         console.log("Conexão com o banco de dados estabelecida...");
-//     }
-// });
+database.connect((erro) => {
+    if (erro) {
+        throw erro;
+    } else {
+        console.log("Conexão com o banco de dados estabelecida...");
+    }
+});
 
 global.database = database;
-module.exports = database;
-
-// const Sequelize = require('sequelize')
-// const sequelize = new Sequelize(database.database, database.user, database.password, {
-//     dialect: 'mysql',
-//     host: database.host
-// })
-
-// module.exports = sequelize
+module.exports = database; 
